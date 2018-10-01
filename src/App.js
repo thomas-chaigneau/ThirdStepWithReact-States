7d37b921e85de34971b92641fc7858fdc11b1ad1
Thomas Chaigneau <thomas.a.chaigneau@gmail.com>
2018-10-01 17:35:10 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+//import Quote from "./Quote";
+import Quotes from "./Quotes";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Simpsons Quotes YO</h1>
+        </header>
+
+        <Quotes/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
